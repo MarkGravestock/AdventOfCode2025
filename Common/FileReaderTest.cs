eeb1cccd9e0cb7b7425628099d1ff0e5dfb0d57d
MarkGravestock <MarkGravestock@users.noreply.github.com>
2025-11-30 11:15:39 +0000

    Make it work across platform

--- Common/FileReaderTest.cs
+++ Common/FileReaderTest.cs
@@ -7,7 +7,7 @@ public class FileReaderTest
     [Fact]
     public void it_reads_the_lines_in_a_file()
     {
-        new FileReader(@".\Input\test.txt").Lines().Should().Contain("A Y", "B X", "C Z");
+        new FileReader(@"./Input/test.txt").Lines().Should().Contain("A Y", "B X", "C Z");
     }
     
     [Fact]
