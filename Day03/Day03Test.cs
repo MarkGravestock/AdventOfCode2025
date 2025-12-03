20bbce6a917c2323ee3eebcb2665e33f2660c3d5
MarkGravestock <MarkGravestock@users.noreply.github.com>
2025-12-03 22:12:53 +0000

    Day 3 Part 2

--- Day03/Day03Test.cs
+++ Day03/Day03Test.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Text;
+using FluentAssertions;
 using MarkGravestock.AdventOfCode2025.Common;
 
 namespace MarkGravestock.AdventOfCode2025.Day03;
@@ -14,14 +15,14 @@ public class Day03Test
         [InlineData("234234234234278", 7, 8)] 
         public void it_can_find_the_joltages(string batteries, int firstJoltage, int secondJoltage)
         {
-            var range = new BatteryBank(batteries);
+            var range = new TwoBatteryBank(batteries);
             range.FirstJoltage.Should().Be(firstJoltage);
             range.SecondJoltage.Should().Be(secondJoltage);
             range.TotalJoltage().Should().Be(firstJoltage * 10 + secondJoltage);
         }
         
         [Fact]
-        public void it_can_find_the_total_total_joltage()
+        public void it_can_find_the_total_test_joltage()
         {
             var sut = new JoltageMaximiser("day3_test.txt");
             sut.TotalJoltage().Should().Be(357);
@@ -37,23 +38,82 @@ public class Day03Test
             sut.TotalJoltage().Should().Be(17155);
         }
     }
+
+    public class Day03Part2
+    {
+        [Theory]
+        [InlineData("987654321111111", 987654321111)]
+        [InlineData("811111111111119", 811111111119)] 
+        [InlineData("234234234234278", 434234234278)]
+        [InlineData("818181911112111", 888911112111)] 
+        public void it_can_find_the_joltages(string batteries, long totalJoltage)
+        {
+            var range = new BatteryBank(batteries, 12);
+            range.TotalJoltage().Should().Be(totalJoltage);
+        }
+        
+        [Fact]
+        public void it_can_find_the_total_test_joltage()
+        {
+            var sut = new JoltageMaximiser("day3_test.txt", 12);
+            sut.TotalJoltage().Should().Be(3121910778619);
+        }
+ 
+        [Fact]
+        public void it_can_find_the_total_joltage()
+        {
+            var sut = new JoltageMaximiser("day3.txt", 12);
+            sut.TotalJoltage().Should().Be(169685670469164L);
+        }
+    }
 }
 
-public class JoltageMaximiser(string fileName)
+public class JoltageMaximiser(string fileName, int numberOfBatteries = 2)
 {
-    private readonly string fileName = fileName;
-
-
-    public int TotalJoltage()
+    public long TotalJoltage()
     {
         return FileReader.FromInput(fileName).Lines()
-            .Select(line => new BatteryBank(line).TotalJoltage()).Sum();
+            .Select(line => new BatteryBank(line, numberOfBatteries).TotalJoltage()).Sum();
     }
 }
 
 public class BatteryBank
 {
-    public BatteryBank(string batteries)
+    private readonly StringBuilder result = new();
+    
+    public BatteryBank(string batteries, int numberOfBatteries = 2)
+    {
+        var nextHighestIndex = 0;
+        var remaining = numberOfBatteries - 1;
+
+        while (remaining >= 0)
+        {
+            var nextHighest = batteries[nextHighestIndex];
+            
+            for (int i = nextHighestIndex + 1; i < batteries.Length - remaining; i++)
+            {
+                if (batteries[i] > nextHighest)
+                {
+                    nextHighest = batteries[i];
+                    nextHighestIndex = i;
+                }
+            }
+
+            nextHighestIndex++;
+            remaining--;
+            result.Append(nextHighest);
+        }
+    }
+
+    public long TotalJoltage()
+    {
+        return long.Parse(result.ToString());
+    }
+}
+
+public class TwoBatteryBank
+{
+    public TwoBatteryBank(string batteries)
     {
         var firstHighestIndex = 0;
         var firstHighest = batteries[firstHighestIndex];
