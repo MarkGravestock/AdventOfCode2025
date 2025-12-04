68b6379ade9d3275d5b9ad021bdde3c2a5350336
MarkGravestock <MarkGravestock@users.noreply.github.com>
2025-12-04 20:35:26 +0000

    Add skeleton for Day05

---
+++ Day05/Day05Test.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+
+namespace MarkGravestock.AdventOfCode2025.Day05;
+
+public class Day05Test
+{
+    public class Day05Example
+    {
+        private readonly string sut = "sut";
+
+        [Fact]
+        public void it_works()
+        {
+            sut.Should().Be("sut");
+        }
+    }
+
+    public class Day04Part1
+    {
+        [Fact]
+        public void it_works()
+        {
+            "sut".Should().Be("sut");
+        }
+    }
+
+    public class Day04Part2
+    {
+        [Fact]
+        public void it_works()
+        {
+            "sut".Should().Be("sut");
+        }
+    }
+}
