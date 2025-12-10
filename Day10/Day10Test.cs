6da78b46755bdc5e79398c7b17c23a56df5b4b76
MarkGravestock <MarkGravestock@users.noreply.github.com>
2025-12-10 20:53:36 +0000

    Day 10 Part 1

--- Day10/Day10Test.cs
+++ Day10/Day10Test.cs
@@ -32,6 +32,26 @@ public class Day10Test
         }
         
         [Fact]
+        public void it_can_solve_second_example()
+        {
+            var machine = new Machine(".....","[...#.]", ["(0,2,3,4)","(2,3)","(0,4)","(0,1,2)","(1,2,3,4)"]);
+            machine.PressButton(2);
+            machine.PressButton(3);
+            machine.PressButton(4);
+            machine.CurrentIndicatorLights().Should().Be("...#.");
+            machine.IsAtDesiredState().Should().Be(true);
+        }
+
+        [Fact]
+        public void it_can_solve_third_example()
+        {
+            var machine = new Machine("......","[.###.#]", ["(0,1,2,3,4)","(0,3,4)","(0,1,2,4,5)","(1,2)"]);
+            machine.PressButton(1);
+            machine.PressButton(2);
+            machine.CurrentIndicatorLights().Should().Be(".###.#");
+            machine.IsAtDesiredState().Should().Be(true);
+        }
+        [Fact]
         public void it_apply_a_button_press_to_machine()
         {
             var machine = sut.Machines().First();
@@ -40,6 +60,21 @@ public class Day10Test
             machine.PressButton(2);
             machine.IsAtDesiredState().Should().Be(true);
         }
+        
+        [Fact]
+        public void it_can_find_the_minimum()
+        {
+            sut.FindMininum(0).Should().Be(2);
+            sut.FindMininum(1).Should().Be(3);
+            sut.FindMininum(2).Should().Be(2);
+        }
+        
+        [Fact]
+        public void it_can_find_the_total_minimum()
+        {
+            sut.FindTotalMininum().Should().Be(7);
+        }
+
     }
 
     public class Day10Part1(ITestOutputHelper output)
@@ -47,9 +82,9 @@ public class Day10Test
         private readonly IndicatorLightDiagram sut = new("day10.txt", output);
 
         [Fact]
-        public void it_can_count_the_machines()
+        public void it_can_find_the_total_minimum()
         {
-            sut.Machines().Count.Should().Be(157);
+            sut.FindTotalMininum().Should().Be(399);
         }
     }
 
@@ -121,13 +156,72 @@ public class IndicatorLightDiagram
         return machines;
     }
 
+    public int FindMininum(int machineIndex)
+    {
+        var machine = machines[machineIndex];
+
+        int minimum = -1;
+        
+        for (var i = 1; i <= machine.ButtonWiring.Length && minimum == -1; i++)
+        {
+            var combinations = GetCombinations(machine.ButtonWiring.Length - 1, i).ToList();
+            
+            for (var j = 0; j < combinations.Length() ; j++)
+            {
+                combinations[j].ForEach(buttonIndex => machine.PressButton(buttonIndex));
+                if (machine.IsAtDesiredState())
+                {
+                    minimum = i;
+                    break;
+                }
+
+                machine.Reset();
+            }
+        }
+
+        return minimum;
+    }
+
+    public static IEnumerable<int[]> GetCombinations(int n, int k)
+    {
+        return GetCombinationsHelper(0, n, k);
+    }
+
+    private static IEnumerable<int[]> GetCombinationsHelper(int start, int n, int k)
+    {
+        if (k == 0)
+        {
+            yield return new int[0];
+            yield break;
+        }
+    
+        for (int i = start; i <= n - k + 1; i++)
+        {
+            foreach (var combo in GetCombinationsHelper(i + 1, n, k - 1))
+            {
+                yield return new[] { i }.Concat(combo).ToArray();
+            }
+        }
+    }
+
+    public int FindTotalMininum()
+    {
+        var total = 0;
+
+        for (int i = 0; i < machines.Count; i++)
+        {
+            total += FindMininum(i);
+        }
+        
+        return total;
+    }
 }
 
 public class Machine 
 {
     public Machine(string initialIndicatorLights, string desiredIndicatorLights, string[] buttonWiring)
     {
-        this.InitialIndicatorLights = initialIndicatorLights;
+        InitialIndicatorLights = initialIndicatorLights;
         this.desiredIndicatorLights = desiredIndicatorLights;
         var wiring = buttonWiring.Select(x => x[1..^1]);
         this.buttonWiring = wiring.Select(BinaryPositionsToDecimal).ToArray();
@@ -148,7 +242,6 @@ public class Machine
     private Lights currentIndicatorLights;
     private readonly string desiredIndicatorLights;
     private readonly int[] buttonWiring;
-    private readonly string joltage;
 
     public string InitialIndicatorLights { get; }
 
@@ -170,5 +263,12 @@ public class Machine
     {
         return CurrentIndicatorLights() == DesiredIndicatorLights;
     }
+
+    public void Reset()
+    {
+        currentIndicatorLights = Lights.From(InitialIndicatorLights);
+    }
 }
+
+
 
